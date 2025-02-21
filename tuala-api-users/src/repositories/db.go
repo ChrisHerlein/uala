941dffc21460aa3839fea5521b40dc250adeb612
Christian Herlein <christian.herlein@cloudx.com>
2025-02-21 18:39:21 -0300

    Add method to users db

--- tuala-api-users/src/repositories/db.go
+++ tuala-api-users/src/repositories/db.go
@@ -45,9 +45,9 @@ func (pg *pgdb) Get(name, password string) (*models.User, error) {
 }
 
 func (pg *pgdb) GetByID(userID uint) (*models.User, error) {
-	user := &models.User{ID: userID}
-	res := pg.db.Where(user).Preload("Follows").First(user)
-	return user, res.Error
+	var user models.User
+	res := pg.db.Where("id = ?", userID).Preload("Follows").First(&user)
+	return &user, res.Error
 }
 
 func (pg *pgdb) Follow(from, to uint) error {
@@ -68,6 +68,13 @@ func (pg *pgdb) Unfollow(from, to uint) error {
 	uf := &models.Follow{}
 	res := pg.db.Delete(uf, "user_refer = ? AND follows = ?", from, to)
 	return res.Error
+}
+
+func (pg *pgdb) GetFollowers(from uint) ([]models.Follow, error) {
+	var uf []models.Follow
+
+	res := pg.db.Where("follows = ?", from).Find(&uf)
+	return uf, res.Error
 }
 
 func NewDB(db *gorm.DB) *pgdb {
