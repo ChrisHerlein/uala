11170eda6fe11f39a6927ed56c1fad23a4d13d03
Christian Herlein <christian.herlein@cloudx.com>
2025-02-23 01:37:33 -0300

    Add docs

--- tuala-worker-cache/model.go
+++ tuala-worker-cache/model.go
@@ -6,15 +6,17 @@ import (
 	"github.com/ChrisHerlein/uala/tuala-api-contents/src/models"
 )
 
+// Will be stored into Redis with key: `{userId}-{page.Order}`
 type feedPage struct {
 	UserName string           `json:"userName"` // user who will receive the content
-	Content  []models.Content `json:"content"`
+	Content  []models.Content `json:"content"`  // Newest should be at begining of slice
 	Order    int              `json:"order"`
 }
 
+// Will be stored into Redis with key: `{userId}-control`
 type control struct {
 	UserName   string    `json:"userName"`
 	MostRecent int       `json:"mostRecent"` // #order of last page
-	SizeOfLast int       `json:"sizeOfLast"`
-	Last       time.Time `json:"last"`
+	SizeOfLast int       `json:"sizeOfLast"` // how many tweets are stored into last page
+	Last       time.Time `json:"last"`       // Refers to the newest content in last page
 }
